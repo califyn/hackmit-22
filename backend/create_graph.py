f95e81aec260bfd5e1d2a2db08f464351e79e9eb
GenericP3rson <genericp3rson@gmail.com>
2022-10-01 17:50:47 -0400

    Added the package endpoints

--- backend/create_graph.py
+++ backend/create_graph.py
@@ -4,4 +4,10 @@ from auth import SECRET
 conn = tg.TigerGraphConnection(host="https://pigeon.i.tgcloud.io/", graphname="Pigeon", gsqlSecret=SECRET)
 conn.getToken(SECRET)
 
-conn.upsertVertex("User", "sample_user", {"username": "sample_user", "password": "sample_password"})
+conn.gsql('''
+
+CREATE GRAPH 
+
+''')
+
+# conn.upsertVertex("User", "sample_user", {"username": "sample_user", "password": "sample_password"})
