4e6229620640f8af055bc176f8f58632241193e6
Serena <serena.an@gmail.com>
2022-10-01 17:04:56 -0400

    beta login buttons

--- frontend/pages/style.js
+++ frontend/pages/style.js
@@ -1,20 +1,22 @@
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Button, Text, View, Image } from 'react-native';
 
 const COLORS = {
   text: 'black',
   white: '#FDFDFD',
+  lightpurple: '#EBE4E9',
   purple: '#DED2DB',
   green: '#B4C5A7',
+  darkgreen: '#93A399',
   gray: '#868387',
   tan: '#BEAB8D',
   brown: '#A57C54',
-  dark: '#34341E',
+  black: '#34341E',
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: COLORS.purple,
+    backgroundColor: COLORS.lightpurple,
     alignItems: 'center',
     justifyContent: 'center',
   },
@@ -22,6 +24,18 @@ const styles = StyleSheet.create({
     height: '30%',
     width: '67.5%',
   },
+  button: {
+    marginBottom: 20,
+    alignItems: "center",
+    width: 100,
+    padding: 10,
+    backgroundColor: COLORS.white,
+  },
+  space: {
+    width: 20,
+    height: 20,
+  }
 });
+
 
 export default styles;
