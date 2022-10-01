ad29cdccc9b7d1f3c4d84ee83874b78a71507369
GenericP3rson <genericp3rson@gmail.com>
2022-10-01 14:42:40 -0400

    Heroku Attempt I

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+# import uvicorn
 
 app = FastAPI()
 
@@ -7,3 +8,5 @@ app = FastAPI()
 async def root():
     return {"message": "Hello World"}
 
+# if __name__ == "__main__":
+#     uvicorn.run(app)
