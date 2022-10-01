bd3d437682cdb0c3404478a40516f0c9e9590015
Ali Cy <califynic@gmail.com>
2022-10-01 13:56:49 -0400

    Added logo and added it to splash, style currently does not work, please vectorize

--- frontend/App.js
+++ frontend/App.js
@@ -1,9 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <Image source={require('./assets/logo.png')} style={styles.splashlogo}/>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
@@ -16,5 +17,8 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  splashlogo: {
+    height: '50%',
   },
 });
