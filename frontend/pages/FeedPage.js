239f8b126cadd9ce09636a1c64e9f2772e6161dc
GenericP3rson <genericp3rson@gmail.com>
2022-10-02 08:37:25 -0400

    All pins sort ofish working

--- frontend/pages/FeedPage.js
+++ frontend/pages/FeedPage.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import MapView from 'react-native-maps';
 import React, { useEffect, useRef, useState } from 'react';
 import { Text, View, Image, Pressable, ScrollView, Animated, TouchableOpacity } from 'react-native';
 import styles from './style'
 
 import { LinearGradient } from 'expo-linear-gradient';
+
+import MapView, { Marker } from 'react-native-maps';
+import * as Location from 'expo-location'
 
 import CenterMap from '../components/CenterMap'
 
@@ -31,6 +33,40 @@ export default function FeedPage({navigation, route}) {
     }
   }
 
+  let mapRef = useRef(null);
+  const [location, setLocation] = useState("");
+  const [markers, setMarkers] = useState([]);
+
+  useEffect(() => {
+        (async () => {
+            let { status } = await Location.requestForegroundPermissionsAsync();
+            if (status !== 'granted') {
+                return;
+            }
+
+            let location = await Location.getCurrentPositionAsync({
+                accuracy: Location.Accuracy.Balanced,
+                enableHighAccuracy: true,
+                timeInterval: 5
+            });
+            setLocation(location);
+
+            mapRef.current.animateToRegion({
+                "latitude":location.coords.latitude - 0.005,
+                "longitude": location.coords.longitude,
+                "latitudeDelta":0.05,
+                "longitudeDelta":0.05
+            });
+            if (markers.length == 0) {
+                setMarkers(markers.concat([{
+                    latitude: location.coords.latitude,
+                    longitude: location.coords.longitude,
+                    title: 'Current Location',
+                }]));
+            }
+        })();
+    }, []);
+
   const feed_test = route.params.locations;
 
 
@@ -46,7 +82,21 @@ export default function FeedPage({navigation, route}) {
            <Image source={require('../assets/plus.png')} style={{width: 30, height: 30, padding: 20}} />
           </TouchableOpacity>
       </View>
-      <CenterMap />
+      <MapView style={styles.map} ref={mapRef}>
+          {feed_test.map((obj) => {
+            return (
+                <MapView.Marker
+                    coordinate={{latitude: obj.lat,
+                    longitude: obj.lon}}
+                    title={obj.id}
+                    description={obj.id}
+                    key={obj.id}
+                 >
+                    <Image source={require('../assets/gift_pin.png')} style={styles.mapMarker}/>
+                 </MapView.Marker>
+            );
+          })}
+      </MapView>
           <APressable onPressOut={() => {if(!feedUp){setFeedAndAnimate(!feedUp);}}} pressRetentionOffset={{top: 500}} disabled={feedUp} style={[styles.feed, {flex: feedUp ? 1 : null, height: realHeight, paddingTop: paddingAnimate}]}>
                   <Text style={styles.h1}>Feed</Text>
                   <ScrollView onScrollEndDrag={(event) => {if(event.nativeEvent.contentOffset.y <= -100) {setFeedAndAnimate(false)}}} style={feedUp ? styles.feedScrollFull : null} showsVerticalScrollIndicator={false}>
